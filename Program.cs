86d3f7c9decbb3b807f8a223fff6f7f4e6766002
ogokdas <ogokdas@yandex.com>
2022-02-04 16:56:31 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifElse
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                int saat = DateTime.Now.Hour;
+
+                switch (saat)
+                {
+                    case 9:
+                        Console.WriteLine("Günaydın");
+                        break;
+                    case 12:
+                        Console.WriteLine("Tünaydın");
+                        break;
+                    case 18:
+                        Console.WriteLine("İyi akşamlar");
+                        break;
+                    default:
+                        Console.WriteLine("Vakit nekadar hızlı geçmiş");
+                        break;
+                }
+
+                Console.ReadLine();
+
+            }
+        }
+    }
